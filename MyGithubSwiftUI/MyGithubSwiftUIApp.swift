e2094e25a597873565ed41a677d312bef9786265
JuraevDev <tezkormedia@gmail.com>
2021-10-21 18:15:38 +0500

    Initial Commit

---
+++ MyGithubSwiftUI/MyGithubSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGithubSwiftUIApp.swift
+//  MyGithubSwiftUI
+//
+//  Created by Asadulla Juraev on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyGithubSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
